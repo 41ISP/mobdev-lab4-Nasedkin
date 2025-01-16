b9d5f9f6bee66721f0d39467c5587f9c53e01207
ShihiroMori <131643237+ShihiroMori@users.noreply.github.com>
2025-01-16 11:17:02 +0000

    v0.124135782

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -5,6 +5,9 @@ import "./SeacrhPage.css"
 const SearchPage = () => {
 
     const [city, setCity] = useState('')
+    const [icon, setIcon] = useState('')
+    const [cords, setCords] = useState('')
+    const [wType, setwType] = useState('')
     const [temp, setTemp] = useState<undefined|number>(undefined)
     const handleChange = (e: { target: { value: SetStateAction<string> } }) =>{
         setCity(e.target.value)
@@ -13,11 +16,18 @@ const SearchPage = () => {
     const handleClick = async () =>{
         const cityKey = (await weatherRequest.cityReq(city))[0].Key
         setTemp((await weatherRequest.weatherReq(cityKey))[0].Temperature.Value)
+        const pic = (await weatherRequest.weatherReq(cityKey))[0].WeatherIcon.toString()
+        setIcon("https://developer.accuweather.com/sites/default/files/"+pic.padStart(2,'0')+"-s.png")
+        setCords((await weatherRequest.cityReq(city))[0].GeoPosition.Latitude.toString() + ", " + (await weatherRequest.cityReq(city))[0].GeoPosition.Longitude.toString())
+        setwType((await weatherRequest.weatherReq(cityKey))[0].IconPhrase.toString().toUpperCase())
     }
 
     return (
         <div className="container">
+            <img className="icon" src={icon} alt="тут будет иконка погоды"/>
+            <p className="wtype">{wType}</p>
             <p className="temperatureText">Temperature: {temp} °C</p>
+            <p>Coordinates: {cords}</p>
             <input className="inp" type="text" onChange={handleChange} value={city}/>
             <button className="btn" onClick={handleClick}>Click</button>
         </div>
